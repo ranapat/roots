cea8b18aac2b4bf70bd84b0df74f22e2b5394dcc
ranapat <ranapat@gmail.com>
2024-05-09 11:50:41 +0200

    sanity

--- roots/src/main/java/org/ranapat/roots/converter/RxExtensions.kt
+++ roots/src/main/java/org/ranapat/roots/converter/RxExtensions.kt
@@ -2,6 +2,6 @@ package org.ranapat.roots.converter
 
 import io.reactivex.rxjava3.core.Maybe
 
-fun <T: Any> Maybe<String>.instance(valueType: Class<T>): Maybe<T> = map { body ->
-    Converter.fromJson(body, valueType)
+fun <T: Any> Maybe<String>.instance(valueType: Class<T>): Maybe<T> = map { string ->
+    Converter.fromJson(string, valueType)
 }
